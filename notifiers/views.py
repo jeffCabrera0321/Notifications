f92962e9983b1221f54f3a1e93560a787e69cb50
jeffCabrera0321 <j.cabrera0321@gmail.com>
2024-08-08 23:22:03 -0400

    Updated for sending emails.

--- notifiers/views.py
+++ notifiers/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 import smtplib
 from email.mime.text import MIMEText
@@ -24,19 +26,26 @@ def sendEmail(request):
 
     msg = MIMEMultipart()
     msg["From"] = os.getenv("email")
-    msg["To"] = request.data["email"]
+    msg["To"] = request.data["Email"]
     msg["Subject"] = "SmartHomie"
-
-    msg.attach(MIMEText(request.data["message"], "plain"))
+    data = {"status": "Success",
+            "name": request.data["Name"],
+            "manufacturer": request.data["Manufacturer"],
+            "versionNumber": request.data["Version Number"],
+            "description": request.data["Description"],
+            "location": request.data["Location"],
+            "message": request.data["Message"]}
+    msg.attach(MIMEText(json.dumps(data, sort_keys=True, indent=4), "plain"))
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
 
     try:
-        server.login(senderInfo["email"], senderInfo["password"])
 
-        server.sendmail(senderInfo["email"], request.data["email"], msg.as_string())
-        return Response(data="Success", status=status.HTTP_200_OK)
+        server.login(senderInfo["email"], senderInfo["password"])
+        server.sendmail(senderInfo["email"], request.data["Email"], msg.as_string())
+
+        return Response(data=json.dumps(data), status=status.HTTP_200_OK)
     except Exception as _:
         return Response(data="Fail", status=status.HTTP_403_FORBIDDEN)
     finally:
